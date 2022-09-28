645f9563204684f51d6588ff942113cbb243620a
ka0000ce <dailyphase@gmail.com>
2022-09-28 10:51:41 +0300

    Добавьте файлы проекта.

---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double I;
+            double U;
+            bool isOK = false;
+            do
+            {
+                Console.WriteLine("Введiть напругу:");
+                string strU = Console.ReadLine();
+                isOK = double.TryParse(strU, out U);
+                if (!isOK)
+                {
+                    Console.WriteLine("Дурко , не правельно!");
+                }
+            }
+            while (!isOK);
+            Console.WriteLine("Введiть опiр:");
+            double strR = double.Parse(Console.ReadLine());
+            if (strR <= 0)
+            {
+                Console.WriteLine("І знову не вірно.");
+                Console.ReadLine();
+                return;
+            }
+            I = U / strR;
+            Console.WriteLine($"Отримуємо : {I}");
+            Console.ReadKey();
+        }
+    }
+}
