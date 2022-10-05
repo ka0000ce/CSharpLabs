5b73bc0a8a5561d0ee885ee7f5594e1033bfbcba
ka0000ce <dailyphase@gmail.com>
2022-10-05 18:15:27 +0300

    Fix Issues

--- lab4/lab4/Program.cs
+++ lab4/lab4/Program.cs
@@ -6,29 +6,31 @@ using System.Threading.Tasks;
 
 namespace lab4
 {
-    class Program {
+    class Program
+    {
 
-static void Main(string[] args)
+        static void Main(string[] args)
         {
             int a = 0;
             int b = 0;
             int c = 0;
 
-            Console.Write ("Введіть довжину масиву: ");
+            Console.Write("Введіть довжину масиву: ");
 
             int[] arr = new int[int.Parse(Console.ReadLine())];
 
-            for (int i = 0; i < arr.Length; i++) {
+            for (int i = 0; i < arr.Length; i++)
+            {
                 Console.Write("Введіть " + i + " елемент масиву = ");
-                arr[i] = int.Parse(Console.ReadLine());               
+                arr[i] = int.Parse(Console.ReadLine());
             }
 
-            for (int i = 0; i < arr.Length ; i++) 
+            for (int i = 0; i < arr.Length; i++)
             {
-          
-                for (int j = 0; j < arr.Length ; j++)
+
+                for (int j = 0; j < arr.Length; j++)
                 {
-                   
+
                     if (arr[i] == arr[j])
                     {
                         c++;
@@ -44,9 +46,9 @@ static void Main(string[] args)
 
                 Console.WriteLine("Найбільше повторень = {0}, кількість повторень = {1}", a, b);
             }
-          
-            
+
+
             Console.ReadKey(true);
         }
-      }
     }
+}
