b759b9af57dd7c7c4205fb9ea43e8deac95b9f52
ka0000ce <dailyphase@gmail.com>
2022-10-05 12:34:38 +0300

    hi new update

---
+++ lab4/lab4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int mostOtenNumber = 0;
+            int highestNumberOfRepetitions = 0;
+            int numberOfRepetitions = 0;
+            Console.Write("Введiть довжину масиву: ");
+
+            int[] arr = new int[int.Parse(Console.ReadLine())];
+
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("Введiть " + i + " елемент масиву = ");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            foreach (int i in arr)
+            {
+                foreach (int j in arr)
+                {
+                    if (i == j)
+                    {
+                        numberOfRepetitions++;
+                    }
+                }
+
+                if (highestNumberOfRepetitions < numberOfRepetitions)
+                {
+                    highestNumberOfRepetitions = numberOfRepetitions;
+                    mostOtenNumber = i;
+                }
+                numberOfRepetitions = 0;
+            }
+            Console.WriteLine("Найбiльше повторюється число {0}, кiлькiсть його повторень становить {1}", mostOtenNumber, highestNumberOfRepetitions);
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ lab5/lab5/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+namespace lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m, n;
+            Console.WriteLine("Введiть кiлькiсть рядкiв: ");
+            m = Convert.ToInt32(Console.ReadLine());
+            
+           
+            Console.WriteLine("Введiть кiлькiсть стовпчикiв: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            
+            
+            int[,] arr = new int[m, n];
+            var random = new Random();
+            
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    arr[i, j] = random.Next(0, 10);
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();    
+            for (int i = 0; i < m; i++)
+            {
+                int min = arr[i, 0];
+                for (int j = 0; j < n; j++)
+                {
+                    if (arr[i, j] < min)
+                    {
+                        min = arr[i, j];
+                    }
+                }
+                Console.WriteLine("Рядок {0} : мiнiмальне значення {1}", i, min);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < m; i++)
+            {
+                int max = int.MinValue;
+                for (int j = 0; j < n; j++)
+                {
+                    if (arr[j, i] > max)
+                    {
+                        max = arr[j, i];
+                    }
+                }
+                Console.WriteLine("Стовпчик {0} : максимальне значення {1}", i, max);
+            }
+            Console.WriteLine();
+        }
+    }
+}
