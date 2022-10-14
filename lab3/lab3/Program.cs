e5e3e73a83222bc72efffa83eb08579d369c82a9
ka0000ce <dailyphase@gmail.com>
2022-10-14 21:45:50 +0300

    AA

---
+++ lab3/lab3/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+using System;
+
+namespace lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[3];
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Введите любое число{i}/2");
+                a[i] = int.Parse(Console.ReadLine());
+            }
+
+            if (a[0] != a[1] && a[1] != a[2] && a[2] != a[0])
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+  
